23bac3a38b3a0e0c8f3d37f5e9b0d4fb577f1923
GrantSteinfeld <grant.steinfeld@ibm.com>
2019-02-25 22:08:40 -0500

    fixed add-coffee controller to return response message instead of file

--- web-app/src/controllers/add-coffee.controller.ts
+++ web-app/src/controllers/add-coffee.controller.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-any */
-import {operation, param, requestBody} from '@loopback/rest';
-import {AddCoffee} from '../models/add-coffee.model';
+import { operation, param, requestBody } from '@loopback/rest';
+import { AddCoffee } from '../models/add-coffee.model';
+import { ResponseMessage } from '../models/response-message.model';
 import { BlockChainModule } from '../blockchainClient';
 
 let blockchainClient = new BlockChainModule.BlockchainClient();
@@ -10,68 +11,73 @@ let blockchainClient = new BlockChainModule.BlockchainClient();
  * A transaction named addCoffee
  */
 export class AddCoffeeController {
-  constructor() {}
+  constructor() { }
 
   /**
-   * 
-   * 
+   *
+   *
 
    * @param requestBody Model instance data
    * @returns Request was successful
    */
-  @operation('post', '/addCoffee')
-  async addCoffeeCreate(@requestBody() requestBody: AddCoffee): Promise<AddCoffee> {
+  @operation('post', '/addCoffee', {
+    responses: {
+      '200': {
+        description: 'ResponseMessage model instance',
+        content: { 'application/json': { schema: { 'x-ts-type': ResponseMessage } } },
+      },
+    },
+  })
+  async addCoffeeCreate(@requestBody() requestBody: AddCoffee): Promise<ResponseMessage> {
 
-    console.log('addcoffee, requestBody: ')
-    console.log(requestBody)
+    try {
+      let networkObj = await blockchainClient.connectToNetwork();
 
-    let networkObj = await blockchainClient.connectToNetwork();
-    console.log('newtork obj: ')
-    console.log(networkObj)
-    let dateStr = new Date().toDateString();
-    // dateStr = dateStr.toDateString();
-    let dataForAddCoffee = {
-      function: 'addCoffee',
-      size: requestBody.size,
-      roast: requestBody.roast,
-      batchState: requestBody.batchState,
-      grower: requestBody.grower,
-      transactionId: requestBody.transactionId,
-      timestamp: dateStr,
-      contract: networkObj.contract
-    };
+      let dateStr = new Date().toDateString();
+      let dataForAddCoffee = {
+        function: 'addCoffee',
+        size: requestBody.size,
+        roast: requestBody.roast,
+        batchState: requestBody.batchState,
+        grower: requestBody.grower,
+        transactionId: requestBody.transactionId,
+        timestamp: dateStr,
+        contract: networkObj.contract
+      };
 
-    var result = await blockchainClient.addCoffee(dataForAddCoffee);
+      await blockchainClient.addCoffee(dataForAddCoffee);
 
-    console.log('result from blockchainClient.submitTransaction in controller: ')
-    console.log(result.toString())
+      let responseMessage: ResponseMessage = new ResponseMessage({ message: 'added Coffee to Blockchain' });
+      return responseMessage;
 
-    //$to do: return blockchain hash or confirmation rather than the request
-    return result;        
+    } catch (error) {
+      let responseMessage: ResponseMessage = new ResponseMessage({ message: error, statusCode: '400' });
+      return responseMessage;
+    }
   }
 
   /**
-   * 
-   * 
+   *
+   *
 
    * @param filter Filter defining fields, where, include, order, offset, and limit - must be a JSON-encoded string ({"something":"value"})
    * @returns Request was successful
    */
   @operation('get', '/addCoffee')
-  async addCoffeeFind(@param({name: 'filter', in: 'query'}) filter: string): Promise<AddCoffee[]> {
+  async addCoffeeFind(@param({ name: 'filter', in: 'query' }) filter: string): Promise<AddCoffee[]> {
     throw new Error('Not implemented');
   }
 
   /**
-   * 
-   * 
+   *
+   *
 
    * @param id Model id
    * @param filter Filter defining fields and include - must be a JSON-encoded string ({"something":"value"})
    * @returns Request was successful
    */
   @operation('get', '/addCoffee/{id}')
-  async addCoffeeFindById(@param({name: 'id', in: 'path'}) id: string, @param({name: 'filter', in: 'query'}) filter: string): Promise<AddCoffee> {
+  async addCoffeeFindById(@param({ name: 'id', in: 'path' }) id: string, @param({ name: 'filter', in: 'query' }) filter: string): Promise<AddCoffee> {
     throw new Error('Not implemented');
   }
 
