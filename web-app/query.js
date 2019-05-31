a020d30fc0cdceacbed60ffaec6cf57d764ab32f
Horea Porutiu <horeaporutiu@gmail.com>
2019-05-30 17:57:02 -0700

    update local connection files to reflect new vscode extension

--- web-app/query.js
+++ web-app/query.js
@@ -3,9 +3,24 @@
 const yaml = require('js-yaml');
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const fs = require('fs');
+const path = require('path');
+
+
+// capture network variables from config.json
+const configPath = path.join(process.cwd(), './../server/config.json');
+const configJSON = fs.readFileSync(configPath, 'utf8');
+const config = JSON.parse(configJSON);
+var connection_file = config.connection_file;
+var userName = config.userName;
+var gatewayDiscovery = config.gatewayDiscovery;
+
+// connect to the connection file
+const ccpPath = path.join(process.cwd(), './../server/' + connection_file);
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
 
 // A wallet stores a collection of identities for use
-const wallet = new FileSystemWallet('./../local_fabric/wallet');
+const wallet = new FileSystemWallet('./../server/wallet');
 
 async function main() {
 
@@ -15,22 +30,18 @@ async function main() {
   // Main try/catch block
   try {
 
-    const identityLabel = 'Admin@org1.example.com';
-    let connectionProfile = yaml.safeLoad(fs.readFileSync('./network.yaml', 'utf8'));
+    const userExists = await wallet.exists(userName);
+    if (!userExists) {
+        console.log('An identity for the user ' + userName + ' does not exist in the wallet');
+        console.log('Run the registerUser.js application before retrying');
+        response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first';
+        return response;
+    }
 
-    let connectionOptions = {
-      identity: identityLabel,
-      wallet: wallet,
-      discovery: {
-        asLocalhost: true
-      }
-    };
-
-    // Connect to gateway using network.yaml file and our certificates in _idwallet directory
-    await gateway.connect(connectionProfile, connectionOptions);
+    // Create a new gateway for connecting to our peer node.
+    await gateway.connect(ccp, { wallet, identity: userName, discovery: gatewayDiscovery });
 
     console.log('Connected to Fabric gateway.');
-
     // Connect to our local fabric
     const network = await gateway.getNetwork('mychannel');
 
@@ -41,7 +52,8 @@ async function main() {
 
     console.log('\nSubmit hello world transaction.');
 
-    let response = await contract.evaluateTransaction('queryAll');
+    let response = await contract.submitTransaction('queryAll');
+    
     console.log(response.toString())
     return response;
 
